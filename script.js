6231ab91643e17ab494c372698acdde997e42727
nandan062001 <nandankumaryadav06@gmail.com>
2025-01-31 16:33:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,188 @@
+ function toggleNestedDropdown() {
+            var nestedDropdown = document.getElementById("nestedDropdown");
+            if (nestedDropdown.style.display === "block") {
+                nestedDropdown.style.display = "none";
+            } else {
+                nestedDropdown.style.display = "block";
+            }
+        }
+
+
+        function toggleNestedDropdown2() {
+            var nestedDropdown = document.getElementById("nestedDropdown2");
+            if (nestedDropdown.style.display === "block") {
+                nestedDropdown.style.display = "none";
+            } else {
+                nestedDropdown.style.display = "block";
+            }
+        }
+
+        function toggleNestedDropdown3() {
+            var nestedDropdown = document.getElementById("nestedDropdown3");
+            if (nestedDropdown.style.display === "block") {
+                nestedDropdown.style.display = "none";
+            } else {
+                nestedDropdown.style.display = "block";
+            }
+        }
+
+        function toggleNestedDropdown4() {
+            var nestedDropdown = document.getElementById("nestedDropdown4");
+            if (nestedDropdown.style.display === "block") {
+                nestedDropdown.style.display = "none";
+            } else {
+                nestedDropdown.style.display = "block";
+            }
+        }
+        function toggleNestedDropdown5() {
+            var nestedDropdown = document.getElementById("nestedDropdown5");
+            if (nestedDropdown.style.display === "block") {
+                nestedDropdown.style.display = "none";
+            } else {
+                nestedDropdown.style.display = "block";
+            }
+        }
+        function toggleNestedDropdown6() {
+            var nestedDropdown = document.getElementById("nestedDropdown6");
+            if (nestedDropdown.style.display === "block") {
+                nestedDropdown.style.display = "none";
+            } else {
+                nestedDropdown.style.display = "block";
+            }
+        }
+        function toggleNestedDropdown7() {
+            var nestedDropdown = document.getElementById("nestedDropdown7");
+            if (nestedDropdown.style.display === "block") {
+                nestedDropdown.style.display = "none";
+            } else {
+                nestedDropdown.style.display = "block";
+            }
+        }
+        function toggleNestedDropdown8() {
+            var nestedDropdown = document.getElementById("nestedDropdown8");
+            if (nestedDropdown.style.display === "block") {
+                nestedDropdown.style.display = "none";
+            } else {
+                nestedDropdown.style.display = "block";
+            }
+        }
+        function toggleNestedDropdown9() {
+            var nestedDropdown = document.getElementById("nestedDropdown9");
+            if (nestedDropdown.style.display === "block") {
+                nestedDropdown.style.display = "none";
+            } else {
+                nestedDropdown.style.display = "block";
+            }
+        }
+        function toggleNestedDropdown10() {
+            var nestedDropdown = document.getElementById("nestedDropdown10");
+            if (nestedDropdown.style.display === "block") {
+                nestedDropdown.style.display = "none";
+            } else {
+                nestedDropdown.style.display = "block";
+            }
+        }
+        function toggleNestedDropdown11() {
+            var nestedDropdown = document.getElementById("nestedDropdown11");
+            if (nestedDropdown.style.display === "block") {
+                nestedDropdown.style.display = "none";
+            } else {
+                nestedDropdown.style.display = "block";
+            }
+        }
+        function toggleNestedDropdown12() {
+            var nestedDropdown = document.getElementById("nestedDropdown12");
+            if (nestedDropdown.style.display === "block") {
+                nestedDropdown.style.display = "none";
+            } else {
+                nestedDropdown.style.display = "block";
+            }
+        }
+        function toggleNestedDropdown13() {
+            var nestedDropdown = document.getElementById("nestedDropdown13");
+            if (nestedDropdown.style.display === "block") {
+                nestedDropdown.style.display = "none";
+            } else {
+                nestedDropdown.style.display = "block";
+            }
+        }
+        function toggleNestedDropdown14() {
+            var nestedDropdown = document.getElementById("nestedDropdown14");
+            if (nestedDropdown.style.display === "block") {
+                nestedDropdown.style.display = "none";
+            } else {
+                nestedDropdown.style.display = "block";
+            }
+        }
+        function toggleNestedDropdown15() {
+            var nestedDropdown = document.getElementById("nestedDropdown15");
+            if (nestedDropdown.style.display === "block") {
+                nestedDropdown.style.display = "none";
+            } else {
+                nestedDropdown.style.display = "block";
+            }
+        }
+        function toggleNestedDropdown16() {
+            var nestedDropdown = document.getElementById("nestedDropdown16");
+            if (nestedDropdown.style.display === "block") {
+                nestedDropdown.style.display = "none";
+            } else {
+                nestedDropdown.style.display = "block";
+            }
+        }
+        function toggleNestedDropdown17() {
+            var nestedDropdown = document.getElementById("nestedDropdown17");
+            if (nestedDropdown.style.display === "block") {
+                nestedDropdown.style.display = "none";
+            } else {
+                nestedDropdown.style.display = "block";
+            }
+        }
+        function toggleNestedDropdown18() {
+            var nestedDropdown = document.getElementById("nestedDropdown18");
+            if (nestedDropdown.style.display === "block") {
+                nestedDropdown.style.display = "none";
+            } else {
+                nestedDropdown.style.display = "block";
+            }
+        }
+        function toggleNestedDropdown19() {
+            var nestedDropdown = document.getElementById("nestedDropdown19");
+            if (nestedDropdown.style.display === "block") {
+                nestedDropdown.style.display = "none";
+            } else {
+                nestedDropdown.style.display = "block";
+            }
+        }
+        function toggleNestedDropdown20() {
+            var nestedDropdown = document.getElementById("nestedDropdown20");
+            if (nestedDropdown.style.display === "block") {
+                nestedDropdown.style.display = "none";
+            } else {
+                nestedDropdown.style.display = "block";
+            }
+        }
+        function toggleNestedDropdown21() {
+            var nestedDropdown = document.getElementById("nestedDropdown21");
+            if (nestedDropdown.style.display === "block") {
+                nestedDropdown.style.display = "none";
+            } else {
+                nestedDropdown.style.display = "block";
+            }
+        }
+        function toggleNestedDropdown22() {
+            var nestedDropdown = document.getElementById("nestedDropdown22");
+            if (nestedDropdown.style.display === "block") {
+                nestedDropdown.style.display = "none";
+            } else {
+                nestedDropdown.style.display = "block";
+            }
+        }
+        function toggleNestedDropdown23() {
+            var nestedDropdown = document.getElementById("nestedDropdown23");
+            if (nestedDropdown.style.display === "block") {
+                nestedDropdown.style.display = "none";
+            } else {
+                nestedDropdown.style.display = "block";
+            }
+        }
